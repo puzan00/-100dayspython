39ee6779faed2f72d73b02c2418006226dc08e2b
puzan00 <68464696+puzan00@users.noreply.github.com>
2023-02-07 13:17:15 +0545

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,19 @@
+# print odd number in the list
+num=[]
+for i in range(1,20,2):
+    num.append(i)
+print(num)
+
+#------------------------------------------------------------------
+# multiply each element by list
+num=[2,3,4]
+result=[item*10 for item in num]
+print(result)
+
+#------------------------------------------------------------------
+#print multiplication of a number and store in the list
+list=[]
+for i in range(1,11):
+    # print(i*3)
+    list.append(i*3)
+print(list)
