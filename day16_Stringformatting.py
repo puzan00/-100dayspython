bfaf38e987c0658e02e2b8d6893af84527c48c02
puzan00 <68464696+puzan00@users.noreply.github.com>
2023-02-15 16:13:47 +0545

    Add files via upload

---
+++ day16_Stringformatting.py
@@ -0,0 +1,7 @@
+#String formatting
+first_name="Ram"
+last_name="Tamang"
+age= 23
+salary= 25000
+print("Hi {} {}.Your age is {}".format(first_name,last_name,age))
+print(f"Hi {first_name} {last_name}.Your age is {age}") 
